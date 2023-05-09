d0abf0f51a94d5f9e3b5187b0a0754b391202b91
Bruno Gago <bruno.gago@ab-inbev.com>
2023-05-09 11:13:50 -0300

    ServiceRegistry Authentication (Spring Security) implemented

---
+++ service-registry/src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.ead.serviceregistry.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+//Classe responsável pela configuração do Spring Security
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Acessa os valores previamente inseridos no app.yaml do ms
+    @Value("${ead.serviceRegistry.username}")
+    private String username;
+
+    @Value("${ead.serviceRegistry.password}")
+    private String password;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()//essa linha e a de baixo dizem que qualquer requisição HTTP deve estar autenticada
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()//desabilita o CSRF para que não haja uma requisoção de um site paralelo, evitando um attacker
+                .formLogin();//abre um formulário inicial na página para que o user seja autenticado antes de acessar
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
