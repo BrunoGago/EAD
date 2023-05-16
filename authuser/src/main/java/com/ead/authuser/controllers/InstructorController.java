2a4ee349aa9810429cac429209f567f428a2b8dd
Bruno Gago <bruno.gago@ab-inbev.com>
2023-05-16 16:14:48 -0300

    Authorization Roles for Instructor

--- authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -12,6 +12,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -31,6 +32,7 @@ public class InstructorController {
     @Autowired
     RoleService roleService;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto){
         Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
--- course/src/main/java/com/ead/course/validations/CourseValidator.java
+++ course/src/main/java/com/ead/course/validations/CourseValidator.java
@@ -1,5 +1,6 @@
 package com.ead.course.validations;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
@@ -7,6 +8,7 @@ import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -23,6 +25,9 @@ public class CourseValidator implements Validator {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
 
     @Override
     public boolean supports(Class<?> aClass) {
@@ -41,25 +46,17 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors){
-        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
-        if(!userModelOptional.isPresent()){
-            errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found!");
-        }
-        if(userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())){
-            errors.rejectValue("userInstructor", "UserInstructorError", "User must be an INSTRUCTOR OR ADMIN!");
-        }
-
-        //Esse bloco era utilizado para comunicação síncrona, mantive ele comentado para implementar o código acima
-/*        ResponseEntity<UserDto> responseUserInstructor;
-        try{
-            responseUserInstructor = authUserClient.getOneUserById(userInstructor);
-            if(responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT)){
-                errors.rejectValue("userInstructor", "UserInstructorError", "User must be an INSTRUCTOR OR ADMIN!");
-            }
-        }catch (HttpStatusCodeException e){
-            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND)){
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
+        if(currentUserId.equals(userInstructor)) {
+            Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+            if (!userModelOptional.isPresent()) {
                 errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found!");
             }
-        }*/
+            if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be an INSTRUCTOR OR ADMIN!");
+            }
+        } else{
+            throw new AccessDeniedException("Forbidden!");
+        }
     }
 }
